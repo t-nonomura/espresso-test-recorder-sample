af8b07d6d92bf80ebde72c5b5e7f8cb88ebd8063
t.nonomura <nonomura0812@gmail.com>
2019-07-27 13:57:20 +0900

    テキストの表示条件を修正

--- app/src/main/java/net/storehouse/nono/espressotestrecordersample/MainActivity.kt
+++ app/src/main/java/net/storehouse/nono/espressotestrecordersample/MainActivity.kt
@@ -19,12 +19,12 @@ class MainActivity : AppCompatActivity() {
         if (isClicked) {
             main_text.text = resources.getString(R.string.main_text_not_clicked)
             main_text2.text = resources.getString(R.string.main_text2_not_clicked)
-            main_text2.visibility = View.INVISIBLE
+            main_text2.visibility = View.VISIBLE
             button.text = resources.getString(R.string.button_text_not_clicked)
         } else {
             main_text.text = resources.getString(R.string.main_text_clicked)
             main_text2.text = resources.getString(R.string.main_text2_clicked)
-            main_text2.visibility = View.VISIBLE
+            main_text2.visibility = View.INVISIBLE
             button.text = resources.getString(R.string.button_text_clicked)
         }
         isClicked = !isClicked
